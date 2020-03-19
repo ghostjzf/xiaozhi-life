92ca6513c30f79fec7584134998ccb0718bdc4bb
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2020-03-19 14:52:09 +0800

    feat(config): add .gitlab-ci.yml

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h2>Welcome To My World!</h2>
+    <h3>Welcome To My World!</h3>
     <p>网站架构：vue + vuex + element + node + mongodb</p>
     <p>项目采用 docker + gitlab-runner 结合的方式完成自动化部署</p>
   </div>
