da23b1869a4af30158b333d1f46cad23469876ca
jiangzhifeng <jiangzhifeng@tigerbrokers.com>
2020-03-19 14:57:59 +0800

    feat(header): 抽离Header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <el-container class>
-    <el-header>
-      <div id="nav">
-        <router-link to="/">网站首页</router-link>|
-        <router-link to="/blog">个人博客</router-link>|
-        <router-link to="/resume">简历</router-link>|
-        <router-link to="/about">联系方式</router-link>
-      </div>
-    </el-header>
+    <Header />
     <el-main>
       <router-view />
     </el-main>
@@ -18,11 +11,13 @@
 </template>
 
 <script>
+import Header from "@/layout/Header";
 import Footer from "@/layout/Footer.vue";
 
 export default {
   name: "App",
   components: {
+    Header,
     Footer
   }
 };
---
+++ src/layout/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-header>
+    <div id="nav">
+      <router-link to="/">网站首页</router-link>|
+      <router-link to="/blog">个人博客</router-link>|
+      <router-link to="/resume">简历</router-link>|
+      <router-link to="/about">联系方式</router-link>
+    </div>
+  </el-header>
+</template>
